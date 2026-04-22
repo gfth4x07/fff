2ed06aa6fed49d65263d0ed5a543233a4e411f02
Gabriel <gabriel.almeida075@gmail.com>
2026-04-22 19:18:09 -0300

    wtf

--- scripts/teste_servo.py
+++ scripts/teste_servo.py
@@ -5,10 +5,7 @@ myFactory = PiGPIOFactory()
 inp = ''
 servo = Servo(19, pin_factory = myFactory)  # Servo conectado no pino 19
 
-while inp == '':
+while True:
     inp = input("Digite o angulo: ")
-    try:
-        inp = int(text)
-        servo.value = inp
-    except ValueError:
-        break
+    inp = int(inp)
+    servo.value = inp
